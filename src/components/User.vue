0fcbb9892852c6aca1a2292d11e73fd315e18e6d
mdahosanhabib <cseahosan@gmail.com>
2020-02-21 17:59:57 +0600

    Using Props for Parent  Child Communication

---
+++ src/components/User.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="component">
+    <h1>This user component</h1>
+    <p>I'm an awesome user!</p>
+    <button @click="changeName">Change My Name</button>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail :name="name"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import UserDetail from "./UserDetail";
+  import UserEdit from "./UserEdit";
+
+  export default {
+     data: function(){
+       return {
+         name: 'Ahosan'
+       }
+     },
+    methods: {
+       changeName: function () {
+          this.name = 'Habib';
+       }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="component">
+    <h3>You may view the user details here</h3>
+    <p>Details</p>
+    <p>User Name: {{ name }}</p>
+  </div>
+</template>
+
+<!--props name is same as what we using on template. because behind the seen this will create the property-->
+
+<script>
+    export default {
+      props: ['name']
+    }
+</script>
+
+<style scoped>
+  div{
+    background-color: lightcoral;
+  }
+</style>
